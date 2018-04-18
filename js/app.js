7d5f5f683196b156c7ed672a5f5daaad7ee6bfa2
Suhas Srinivasa Bhat <sbhat1@hawk.iit.edu>
2018-04-18 12:14:34 -0500

    Using dataBinding With..in index.html and self in app.js

--- js/app.js
+++ js/app.js
@@ -26,10 +26,11 @@ var Cat = function(){
     };
 }
 var ViewModel= function(){
+    var self=this;
     this.currentCat = ko.observable(new Cat());
     this.incrementCounter= function() {
-        this.currentCat().clickCount(this.currentCat().clickCount()+1);
-      this.currentCat().findLevel();
+        self.currentCat().clickCount(self.currentCat().clickCount()+1);
+      self.currentCat().findLevel();
     };
    
     
